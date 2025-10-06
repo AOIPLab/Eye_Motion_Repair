a1ec2be50e67152dd76a90488397638d9a423aff
Jenna Grieshop <47341279+jengriesh@users.noreply.github.com>
2025-10-06 14:04:53 -0500

    Bug fix - trim function called at end, not as it goes
    It was re-trimming all images in the repair folder after each image was repaired. Now it only calls the trim function once after all have been repaired.

--- Single_Script/Scanner_Distortion_Removal.py
+++ Single_Script/Scanner_Distortion_Removal.py
@@ -246,7 +246,7 @@ for thisfile in os.listdir(dmp_folder_path):
                     mat_engi.Eye_Motion_Distortion_Repair(image_folder_path, image, rois.tolist(),
                                                           shift_array.tolist(), static_distortion, nargout=0)
 
-                mat_engi.trim_emr_edges(image_folder_path + "/Repaired")
+
 
                 # progo.step()
 
@@ -261,7 +261,7 @@ for thisfile in os.listdir(dmp_folder_path):
                 # Write the failed one to the sheet
                 csv_writer.writerow([thisfile])
 
-
+mat_engi.trim_emr_edges(image_folder_path + "/Repaired")
 
 
 root.destroy()
